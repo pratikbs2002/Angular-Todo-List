169b407004677986a6c636a34e64e933c5c52a30
pratikbs2002 <20ce141@charusat.edu.in>
2024-01-28 19:19:11 +0530

    alert modification

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css',
 })
@@ -12,20 +13,22 @@ export class TaskFormComponent {
   taskList: { taskTitle: string; taskId: string }[] = [];
   taskTitle: string = '';
   taskId: string = '';
+  valid: boolean = true;
   addTask = (task: any) => {
     if (task.value['task-title']) {
       this.taskList.push({
         taskTitle: task.value['task-title'],
         taskId: Math.random().toString(),
       });
+      this.valid = true;
       alert('Task added successfully');
     } else {
-      alert('Please enter a task title');
+      this.valid = false;
     }
   };
   constructor() {
-    setInterval(() => {
-      console.log(this.taskList);
-    }, 1000);
+    // setInterval(() => {
+    //   console.log(this.taskList);
+    // }, 1000);
   }
 }
